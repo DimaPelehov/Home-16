813b25dffecb19902f48b3e171a2259c4e429789
Dima <pelexov.mitya@gmail.com>
2023-08-10 20:04:53 +0300

    create DZ 16.2-16.4

---
+++ app.js
@@ -0,0 +1,41 @@
+// -------------------------- ДЗ 16.2-----------------------------
+
+let massive = [1, 2, 3, 4, 5, 6, 7];
+
+function arrSum(arr) {
+  let sum = 0;
+  for (let i of arr) {
+    sum += i;
+  }
+  console.log(sum);
+}
+arrSum(massive);
+// --------------------------ДЗ 16.3---------------------------------------
+
+function fizzbuzz() {
+  for (let i = 1; i <= 50; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      alert("fizzbuzz");
+    } else if (i % 5 == 0) {
+      alert("buzz");
+    } else if (i % 3 == 0) {
+      alert("fizz");
+    }
+    console.log(i);
+  }
+}
+
+fizzbuzz();
+// ------------------------ДЗ 16.4-------------------------------------------------
+function rangeNumbers(a, b) {
+  if (b > a) {
+    for (let i = a; i <= b; i++) {
+      if (i % 2 == 0) continue;
+      alert(i);
+    }
+  } else {
+    alert("В повинно бути більше А");
+  }
+}
+
+rangeNumbers(2, 12);
